481e198de2d4a3446e1ce2c727024d559a70f0e4
Paul DeVito <pdevito3@gmail.com>
2023-04-22 16:49:45 -0400

    update: sort test

--- QueryKit.IntegrationTests/Tests/DatabaseSortingTests.cs
+++ QueryKit.IntegrationTests/Tests/DatabaseSortingTests.cs
@@ -8,6 +8,49 @@ using Microsoft.EntityFrameworkCore;
 public class DatabaseSortingTests : TestBase
 {
     [Fact]
+    public async Task can_sort_items_with_mixed_order_directions()
+    {
+        // Arrange
+        var testingServiceScope = new TestingServiceScope();
+        var fakePersonOne = new FakeTestingPersonBuilder()
+            .WithTitle("alpha")
+            .WithAge(10)
+            .WithBirthMonth("January")
+            .Build();
+        var fakePersonTwo = new FakeTestingPersonBuilder()
+            .WithTitle("beta")
+            .WithAge(100)
+            .WithBirthMonth("February")
+            .Build();
+        var fakePersonThree = new FakeTestingPersonBuilder()
+            .WithTitle("beta")
+            .WithAge(50)
+            .WithBirthMonth("March")
+            .Build();
+        var fakePersonFour = new FakeTestingPersonBuilder()
+            .WithTitle("beta")
+            .WithAge(20)
+            .WithBirthMonth("April")
+            .Build();
+        await testingServiceScope.InsertAsync(fakePersonOne, fakePersonTwo, fakePersonThree, fakePersonFour);
+
+        var input = $"Title desc, Age asc, BirthMonth desc";
+
+        // Act
+        var queryablePeople = testingServiceScope.DbContext().People
+            .Where(x => new[] { fakePersonOne.Id, fakePersonTwo.Id, fakePersonThree.Id, fakePersonFour.Id }.Contains(x.Id));
+        var appliedQueryable = queryablePeople.ApplyQueryKitSort(input);
+        var people = await appliedQueryable.ToListAsync();
+
+        // Assert
+        people.Count.Should().Be(4);
+        people[0].Id.Should().Be(fakePersonFour.Id);
+        people[1].Id.Should().Be(fakePersonThree.Id);
+        people[2].Id.Should().Be(fakePersonTwo.Id);
+        people[3].Id.Should().Be(fakePersonOne.Id);
+    }
+    
+    [Fact]
     public async Task can_sort_multiple_items()
     {
         // Arrange
@@ -67,5 +110,48 @@ public class DatabaseSortingTests : TestBase
         people.Count.Should().Be(2);
         people[0].Id.Should().Be(fakePersonOne.Id);
         people[1].Id.Should().Be(fakePersonTwo.Id);
+    }
+    
+    [Fact]
+    public async Task can_sort_items_with_null_value_property()
+    {
+        // Arrange
+        var testingServiceScope = new TestingServiceScope();
+        var fakePersonOne = new FakeTestingPersonBuilder()
+            .WithTitle("alpha")
+            .WithAge(10)
+            .WithBirthMonth(null)
+            .Build();
+        var fakePersonTwo = new FakeTestingPersonBuilder()
+            .WithTitle("alpha")
+            .WithAge(100)
+            .WithBirthMonth("January")
+            .Build();
+        var fakePersonThree = new FakeTestingPersonBuilder()
+            .WithTitle("beta")
+            .WithAge(50)
+            .WithBirthMonth("March")
+            .Build();
+        var fakePersonFour = new FakeTestingPersonBuilder()
+            .WithTitle("beta")
+            .WithAge(20)
+            .WithBirthMonth("February")
+            .Build();
+        await testingServiceScope.InsertAsync(fakePersonOne, fakePersonTwo, fakePersonThree, fakePersonFour);
+
+        var input = $"Title asc, BirthMonth asc";
+
+        // Act
+        var queryablePeople = testingServiceScope.DbContext().People
+            .Where(x => new[] { fakePersonOne.Id, fakePersonTwo.Id, fakePersonThree.Id, fakePersonFour.Id }.Contains(x.Id));
+        var appliedQueryable = queryablePeople.ApplyQueryKitSort(input);
+        var people = await appliedQueryable.ToListAsync();
+
+        // Assert
+        people.Count.Should().Be(4);
+        people[0].Id.Should().Be(fakePersonTwo.Id); 
+        people[1].Id.Should().Be(fakePersonOne.Id); // null BirthMonth
+        people[2].Id.Should().Be(fakePersonFour.Id);
+        people[3].Id.Should().Be(fakePersonThree.Id);
     }
 }
