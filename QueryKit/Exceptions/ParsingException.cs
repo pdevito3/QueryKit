d905c44801ec8146155b45beb845edf008bffd42
Paul DeVito <pdevito3@gmail.com>
2024-06-22 14:49:26 -0400

    feat: parsing exception shows details of the particular failure

--- QueryKit/Exceptions/ParsingException.cs
+++ QueryKit/Exceptions/ParsingException.cs
@@ -3,7 +3,9 @@ namespace QueryKit.Exceptions;
 public sealed class ParsingException : Exception
 {
     public ParsingException(Exception exception)
-        : base($"There was a parsing failure, likely due to an invalid comparison or logical operator. You may also be missing double quotes surrounding a string or guid.", exception)
+        : base(@$"There was a parsing failure, likely due to an invalid comparison or logical operator. You may also be missing double quotes surrounding a string or guid.
+
+{exception.Message}", exception)
     {
     }
 }
