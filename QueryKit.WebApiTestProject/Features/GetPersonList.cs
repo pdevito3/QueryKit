da3d853f91e51bafeb2fb8a988f3dafff9b0b2b0
Paul DeVito <pdevito3@gmail.com>
2024-06-23 12:08:01 -0400

    refactor: feature

--- QueryKit.WebApiTestProject/Features/GetPersonList.cs
+++ QueryKit.WebApiTestProject/Features/GetPersonList.cs
@@ -8,32 +8,13 @@ using Microsoft.EntityFrameworkCore;
 
 public static class GetPersonList
 {
-    public sealed class Query : IRequest<List<TestingPerson>>
+    public sealed record Query(string FilterInput, string SortInput, IQueryKitConfiguration QueryKitConfig = default) : IRequest<List<TestingPerson>>;
+
+    public sealed class Handler(TestingDbContext testingDbContext) : IRequestHandler<Query, List<TestingPerson>>
     {
-        public readonly string FilterInput;
-        public readonly string SortInput;
-        public readonly IQueryKitConfiguration QueryKitConfig;
-
-        public Query(string filterInput, string sortInput, IQueryKitConfiguration queryKitConfig = default)
-        {
-            FilterInput = filterInput;
-            SortInput = sortInput;
-            QueryKitConfig = queryKitConfig;
-        }
-    }
-
-    public sealed class Handler : IRequestHandler<Query, List<TestingPerson>>
-    {
-        private readonly TestingDbContext _testingDbContext;
-
-        public Handler(TestingDbContext testingDbContext)
-        {
-            _testingDbContext = testingDbContext;
-        }
-
         public async Task<List<TestingPerson>> Handle(Query request, CancellationToken cancellationToken)
         {
-            var queryablePeople = _testingDbContext.People;
+            var queryablePeople = testingDbContext.People;
             var appliedQueryable = queryablePeople
                 .ApplyQueryKitFilter(request.FilterInput, request.QueryKitConfig)
                 .ApplyQueryKitSort(request.SortInput, request.QueryKitConfig);
